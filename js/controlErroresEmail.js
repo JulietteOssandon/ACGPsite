42f5c90aa94dfa0dfbd70dad8616198d1a4e86a5
JulietteOssandon <106613003+JulietteOssandon@users.noreply.github.com>
2022-12-15 23:41:59 -0300

    email alert

---
+++ js/controlErroresEmail.js
@@ -0,0 +1,15 @@
+function validacionForm() {
+    let _nombre = $("#NombreId").val();
+    let _telefono = $("#TelefonoId").val();
+    let _correo = $("#CorreoId").val();
+    let _asunto = $("#AsuntoId").val();
+    let _mensaje = $("#textAreaExample1").val();
+
+    if (_nombre.trim() == "" || _nombre.trim() == "") {
+        
+    } else {
+        
+        alert(error);
+    }
+    
+}
